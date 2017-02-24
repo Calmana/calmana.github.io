48545ec37c7c2572ff7005e4ea99e3a2e2d03ed2
keikreutler <kei@ourmachine.net>
2017-02-24 13:49:59 +0300

    iFrame animation hover added.

--- src/render/scripts/script.js
+++ src/render/scripts/script.js
@@ -1,11 +1,23 @@
-$('#inner-hover-1a').click(function(e){    
-	$('#card-1a').fadeIn('slow');
+$('#inner-hover-1a').hover(function(e){    
+	$('#card-1a').delay( 1200 ).fadeIn('slow');
 });
 
-$('#inner-hover-1b').click(function(e){    
-	$('#card-1b').fadeIn('slow');
+$('#inner-hover-1a').hover(function(e){    
+	$('#iframe-1a').fadeIn(1200);
 });
 
-$('#inner-hover-1c').click(function(e){    
-	$('#card-1c').fadeIn('slow');
+$('#inner-hover-1b').hover(function(e){    
+	$('#card-1b').delay( 1200 ).fadeIn('slow');
+});
+
+$('#inner-hover-1b').hover(function(e){    
+	$('#iframe-1b').fadeIn(1200);
+});
+
+$('#inner-hover-1c').hover(function(e){    
+	$('#card-1c').delay( 1200 ).fadeIn('slow');
+});
+
+$('#inner-hover-1c').hover(function(e){    
+	$('#iframe-1c').fadeIn(1200);
 });
