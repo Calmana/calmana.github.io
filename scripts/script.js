8e02d5ead3cbe6eea029fb2b623c79c3188ba806
keikreutler <kei@ourmachine.net>
2017-02-24 16:55:22 +0300

    bb45e21 Final v1

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+$('.view-0').ripples({
+	resolution: 512,
+	dropRadius: 15,
+	perturbance: 0.01,
+});
+
+$('#inner-hover-1a').hover(function(e){    
+	$('#card-1a').delay( 1000 ).fadeIn('slow');
+});
+
+$('#inner-hover-1a').hover(function(e){    
+	$('#iframe-1a').fadeIn(1000);
+});
+
+$('#inner-hover-1b').hover(function(e){    
+	$('#card-1b').delay( 1000 ).fadeIn('slow');
+});
+
+$('#inner-hover-1b').hover(function(e){    
+	$('#iframe-1b').fadeIn(1000);
+});
+
+$('#inner-hover-1c').hover(function(e){    
+	$('#card-1c').delay( 1000 ).fadeIn('slow');
+});/*
+
+$('#inner-hover-1c').hover(function(e){    
+	$('#iframe-1c').fadeIn(1000);
+});*/
